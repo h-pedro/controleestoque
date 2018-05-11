d37f4cbf5d7b75f31167e046dcbb1a6bc953c195
h-pedro <h-pedro@hotmail.com>
2018-05-10 22:15:11 -0300

    movimentacao

---
+++ WinForms/ExForms.WinUI/FormCadastroMovimentacao.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ExForms.WinUI
+{
+    partial class FormCadastroMovimentacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.rdbE = new System.Windows.Forms.RadioButton();
+            this.rdbS = new System.Windows.Forms.RadioButton();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduto.Location = new System.Drawing.Point(26, 29);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(58, 16);
+            this.lblProduto.TabIndex = 0;
+            this.lblProduto.Text = "Produto:";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblData.Location = new System.Drawing.Point(26, 69);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(40, 16);
+            this.lblData.TabIndex = 1;
+            this.lblData.Text = "Data:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(26, 114);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(39, 16);
+            this.lblTipo.TabIndex = 4;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // rdbE
+            // 
+            this.rdbE.AutoSize = true;
+            this.rdbE.Location = new System.Drawing.Point(29, 133);
+            this.rdbE.Name = "rdbE";
+            this.rdbE.Size = new System.Drawing.Size(77, 17);
+            this.rdbE.TabIndex = 5;
+            this.rdbE.TabStop = true;
+            this.rdbE.Text = "ENTRADA";
+            this.rdbE.UseVisualStyleBackColor = true;
+            // 
+            // rdbS
+            // 
+            this.rdbS.AutoSize = true;
+            this.rdbS.Location = new System.Drawing.Point(113, 133);
+            this.rdbS.Name = "rdbS";
+            this.rdbS.Size = new System.Drawing.Size(57, 17);
+            this.rdbS.TabIndex = 6;
+            this.rdbS.TabStop = true;
+            this.rdbS.Text = "SAIDA";
+            this.rdbS.UseVisualStyleBackColor = true;
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(111, 184);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(100, 20);
+            this.txtQtd.TabIndex = 8;
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantidade.Location = new System.Drawing.Point(25, 188);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(81, 16);
+            this.lblQuantidade.TabIndex = 7;
+            this.lblQuantidade.Text = "Quantidade:";
+            // 
+            // FormCadastroMovimentacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.rdbS);
+            this.Controls.Add(this.rdbE);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.lblProduto);
+            this.Name = "FormCadastroMovimentacao";
+            this.Text = "FormCadastroMovimentacao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.RadioButton rdbE;
+        private System.Windows.Forms.RadioButton rdbS;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Label lblQuantidade;
+    }
+}
---
+++ WinForms/ExForms.WinUI/FormListaMovimentacao.cs
@@ -0,0 +1,148 @@
+﻿using ExForms.DataAccess;
+using ExForms.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExForms.WinUI
+{
+    public partial class FormListaMovimentacao : Form
+    {
+        public FormListaMovimentacao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            var frm = new FormCadastroMovimentacao();
+            if (frm.ShowDialog() != DialogResult.OK)
+                return;
+            CarregarGridView();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            Excluir();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            CarregarGridView();
+        }
+
+        private void FormListaMovimentacao_Load(object sender, EventArgs e)
+        {
+            CarregarGridView();
+        }
+
+        private void mnuNovo_Click(object sender, EventArgs e)
+        {
+            var frm = new FormCadastroMovimentacao();
+            if (frm.ShowDialog() != DialogResult.OK)
+                return;
+            CarregarGridView();
+        }
+
+        private void mnuEditar_Click(object sender, EventArgs e)
+        {
+            Editar();
+        }
+
+        private void mnuExcluir_Click(object sender, EventArgs e)
+        {
+            Excluir();
+        }
+
+        private void gridView_DoubleClick(object sender, EventArgs e)
+        {
+            Editar();
+        }
+
+        private void gridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            gridView.Rows[e.RowIndex].ReadOnly = true;
+        }
+
+        private void txtBusca_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                CarregarGridView();
+            }
+        }
+
+        private void gridView_SelectionChanged(object sender, EventArgs e)
+        {
+            HabilitarBotoes((gridView.SelectedRows.Count == 1), (gridView.SelectedRows.Count > 1));
+        }
+
+        private void HabilitarBotoes(bool oneItemSelected, bool manyItemsSelected)
+        {
+            //btnEditar.Enabled = mnuEditar.Enabled = oneItemSelected;
+            //btnExcluir.Enabled = mnuExcluir.Enabled = (oneItemSelected || manyItemsSelected);
+        }
+
+        private void CarregarGridView()
+        {
+            var lst = new MovimentacaoDAO().BuscarPorTexto(txtBusca.Text);
+            gridView.AutoGenerateColumns = false;
+            gridView.DataSource = lst;
+            HabilitarBotoes((gridView.SelectedRows.Count == 1), (gridView.SelectedRows.Count > 1));
+            gridView.ClearSelection();
+        }
+
+        private void Editar()
+        {
+            if (gridView.SelectedRows.Count > 0)
+            {
+                var id = Convert.ToInt32(gridView.SelectedRows[0].Cells[0].Value);
+                var obj = new MovimentacaoDAO().BuscarPorId(id);
+                //var frm = new FormCadastroMovimentacao(obj);
+                //if (frm.ShowDialog() != DialogResult.OK)
+                //    return;
+                CarregarGridView();
+            }
+        }
+
+
+        private void Excluir()
+        {
+            if (gridView.SelectedRows.Count > 0)
+            {
+                if (MessageBox.Show("Deseja realmente remover o registro selecionado?", "Excluir", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+                    return;
+                var id = Convert.ToInt32(gridView.SelectedRows[0].Cells[0].Value);
+                new MovimentacaoDAO().Deletar(new Movimentacao() { Id = id });
+                CarregarGridView();
+            }
+        }
+
+        private void mnuAcoes_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void gridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
